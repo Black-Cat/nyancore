f5cb5e1cc2ae07d9643a3c78923c441b401b14fd
Black Cat <iblackcatw@gmail.com>
2024-02-03 15:56:00 +0000

    Add asset for gameplay controller

--- src/gameplay/gameplay_controller.zig
+++ src/gameplay/gameplay_controller.zig
@@ -1,17 +1,34 @@
 const std = @import("std");
 
 const Application = @import("../application/application.zig").Application;
+const AssetMap = @import("../application/asset.zig").AssetMap;
+const GameplaySystem = @import("gameplay_system.zig").GameplaySystem;
 const System = @import("../system/system.zig").System;
 
 pub const GameplayController = struct {
     name: []const u8,
     system: System,
 
+    allocator: std.mem.Allocator,
+    gameplay_systems: std.ArrayList(GameplaySystem),
+    gameplay_systems_paths: std.ArrayList([]const u8),
+
     pub fn init(self: *GameplayController, comptime name: []const u8, allocator: std.mem.Allocator) void {
-        _ = allocator;
+        self.allocator = allocator;
         self.name = name;
 
         self.system = System.create(name ++ " System", systemInit, systemDeinit, systemUpdate);
+
+        self.gameplay_systems = std.ArrayList(GameplaySystem).init(self.allocator);
+        self.gameplay_systems_paths = std.ArrayList([]const u8).init(self.allocator);
+    }
+
+    pub fn deinit(self: *GameplayController) void {
+        self.gameplay_systems.deinit();
+
+        for (self.gameplay_systems_paths) |p|
+            self.allocator.free(p);
+        self.gameplay_systems_paths.deinit();
     }
 
     fn systemInit(system: *System, app: *Application) void {
@@ -29,5 +46,30 @@ pub const GameplayController = struct {
         _ = elapsed_time;
         const self: *GameplayController = @fieldParentPtr(GameplayController, "system", system);
         _ = self;
+    }
+
+    pub fn generateAssetMap(self: *GameplayController, allocator: std.mem.Allocator) AssetMap {
+        var pathes: []u8 = std.mem.join(allocator, "\n", self.gameplay_systems_paths.items) catch unreachable;
+
+        var map: AssetMap = AssetMap.init(allocator);
+        map.put("pathes", pathes) catch unreachable;
+        return map;
+    }
+
+    pub fn deinitAssetMap(map: *AssetMap) void {
+        map.allocator.free(map.get("pathes") orelse unreachable);
+        map.deinit();
+    }
+
+    pub fn createFromAssetMap(map: *AssetMap, comptime name: []const u8, allocator: std.mem.Allocator) GameplayController {
+        var gameplay_controller: GameplayController = undefined;
+        gameplay_controller.init(name, allocator);
+
+        var pathes: []u8 = map.get("pathes") orelse unreachable;
+        var it = std.mem.tokenizeSequence(u8, pathes, "\n");
+        while (it.next()) |path|
+            gameplay_controller.gameplay_systems_paths.append(allocator.dupe(path) catch unreachable) catch unreachable;
+
+        return gameplay_controller;
     }
 };
