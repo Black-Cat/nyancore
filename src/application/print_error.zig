1e792b5f0986bdaa0138d83b4b3410d70c6b8705
Black Cat <iblackcatw@gmail.com>
2024-03-09 13:15:09 +0000

    Add printing error with zig error union

--- src/application/print_error.zig
+++ src/application/print_error.zig
@@ -20,3 +20,21 @@ pub fn printError(comptime module: []const u8, error_message: []const u8) void {
     if (builtin.mode == .Debug)
         @panic(error_message);
 }
+
+pub fn printZigErrorNoPanic(comptime module: []const u8, comptime error_message: []const u8, err: anyerror) void {
+    @setCold(true);
+
+    const stdout = std.io.getStdOut().writer();
+    stdout.print("\x1b[1;31m{s} ERROR:\x1b[0m {s} {{{any}}}\n", .{ module, error_message, err }) catch unreachable;
+
+    if (nyancore_options.panic_on_all_errors)
+        @panic(error_message);
+}
+
+pub fn printZigError(comptime module: []const u8, comptime error_message: []const u8, err: anyerror) void {
+    @setCold(true);
+    printZigErrorNoPanic(module, error_message, err);
+
+    if (builtin.mode == .Debug)
+        @panic(error_message);
+}
