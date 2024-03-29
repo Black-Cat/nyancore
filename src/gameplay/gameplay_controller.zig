a99154be7ba1281b26ba80d4ac0797fb8ff2df35
Black Cat <iblackcatw@gmail.com>
2024-03-29 09:49:26 +0000

    Load gameplay systems from default folder

--- src/gameplay/gameplay_controller.zig
+++ src/gameplay/gameplay_controller.zig
@@ -1,11 +1,15 @@
 const std = @import("std");
 
+const build_options = @import("nyancore_options");
+
 const Application = @import("../application/application.zig").Application;
 const AssetMap = @import("../application/asset.zig").AssetMap;
 const GameplaySystem = @import("gameplay_system.zig").GameplaySystem;
 const System = @import("../system/system.zig").System;
 
 pub const GameplayController = struct {
+    pub const DEFAULT_SYSTEMS_FOLDER: []const u8 = "./gameplay_systems/";
+
     name: []const u8,
     system: System,
 
@@ -20,6 +24,8 @@ pub const GameplayController = struct {
         self.system = System.create(name ++ " System", systemInit, systemDeinit, systemUpdate);
 
         self.gameplay_systems = std.ArrayList(GameplaySystem).init(self.allocator);
+
+        self.loadSystemsFromFolder(DEFAULT_SYSTEMS_FOLDER);
     }
 
     pub fn deinit(self: *GameplayController) void {
@@ -48,5 +54,29 @@ pub const GameplayController = struct {
         gs.init(name, self.allocator);
 
         return gs;
+    }
+
+    pub fn loadSystemsFromFolder(self: *GameplayController, path: []const u8) void {
+        if (!build_options.dev_build)
+            @compileError("Can't load gameplay systems from zig files in non dev build");
+
+        const cwd: std.fs.Dir = std.fs.cwd();
+        var folder: std.fs.IterableDir = cwd.openIterableDir(path, .{}) catch unreachable;
+        defer folder.close();
+
+        var walker: std.fs.IterableDir.Walker = folder.walk(self.allocator) catch unreachable;
+        defer walker.deinit();
+
+        while (walker.next() catch unreachable) |entry| {
+            if (entry.kind != .file)
+                continue;
+
+            if (!std.mem.endsWith(u8, entry.basename, ".zig"))
+                continue;
+
+            const gs: *GameplaySystem = self.gameplay_systems.addOne() catch unreachable;
+            gs.init(entry.basename[0 .. entry.basename.len - ".zig".len], self.allocator);
+            gs.path = gs.allocator.dupe(u8, entry.path) catch unreachable;
+        }
     }
 };
